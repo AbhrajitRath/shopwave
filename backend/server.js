c87d04fc1ad39ff7e220fc30ff53e78c61abd94b
Abhrajit Rath <iamabhrajitrath@gmail.com>
2026-02-25 04:05:04 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const userRoutes = require('./routes/users');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
